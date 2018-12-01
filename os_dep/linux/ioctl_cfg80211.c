a5ac572d2ce95af1c61a1bf6bab7796fd61bec6c
Hans Ulli Kroll <ulli.kroll@googlemail.com>
2018-12-01 17:49:03 +0100

    cfg80211_rtw_change_iface
    Signed-off-by: Hans Ulli Kroll <ulli.kroll@googlemail.com>

--- os_dep/linux/ioctl_cfg80211.c
+++ os_dep/linux/ioctl_cfg80211.c
@@ -1915,7 +1915,10 @@ enum nl80211_iftype {
 */
 static int cfg80211_rtw_change_iface(struct wiphy *wiphy,
 				     struct net_device *ndev,
-				     enum nl80211_iftype type, u32 *flags,
+				     enum nl80211_iftype type,
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(4,12,0))
+				     u32 *flags,
+#endif
 				     struct vif_params *params)
 {
 	enum nl80211_iftype old_type;
